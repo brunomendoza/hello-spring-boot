d4b6fc5c54cad9b31769aa488c8a06d74efc0c6a
Bruno Mendoza <hola@brunomendoza.es>
2022-03-25 17:37:09 +0000

    Initial commit

---
+++ src/test/java/es/brunomendoza/lab/hellospringboot/HelloSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package es.brunomendoza.lab.hellospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
